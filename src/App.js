fc597eecf0f0938b4693f094822151154600bbc6
Winnie-Kiara <winniekk20@gmail.com>
2022-02-06 19:27:47 +0300

    adds fetching with axios

--- src/App.js
+++ src/App.js
@@ -1,63 +1,57 @@
-import './App.css';
-import {Table} from 'react-bootstrap';
-import {React, useEffect, useState} from 'react';
+import "./App.css";
+import { Table } from "react-bootstrap";
+import api from "./Api/API"
+import { React, useEffect, useState } from "react";
 function App() {
-  const [data,setData]=useState([]);
+  const [documents, setDocuments] = useState([]);
 
-  const getData=()=>{
-    fetch('sample.json'
-    ,{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }
-    )
-      .then(function(response){
-        console.log(response)
-        return response.json();
-      })
-      .then(function(myJson) {
-        console.log(myJson);
-        setData(myJson)
-      });
-  }
   useEffect(()=>{
-    getData()
-  },[])
-
+    const fetchDocuments = async() =>{
+      try{
+        const response = await api.get('/Documents');
+        setDocuments(response.data);
+      } catch(err){
+        if (err.response){
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else{
+          console.log(`Error:${err.message}`);
+        }
+      }
+    }
+    fetchDocuments();
+  }, [setDocuments])
   return (
     <div className="App">
-      {
-       data && data.length>0 && data.map(({owner, indexNo})=>
-
+        
       <Table striped bordered hover size="sm">
-{indexNo === "11" ? (
-
-<h1>{indexNo}</h1>
-):null}
-  <thead>
-    <tr>
-      <th>IndexNo</th>
-      <th>DocumentName</th>
-      <th>DocumentSize</th>
-      <th>Owner</th>
-      <th>Download</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    
-  </tbody>
-      
-</Table>
-)
-}
+       
+        <thead >
+     
+          <tr>
+            <th>IndexNo</th>
+            <th>DocumentName</th>
+            <th>DocumentSize</th>
+            <th>Owner</th>
+            <th>Download</th>
+          </tr>
+       
+        </thead>
+        <tbody>
+        {documents && documents.map((Documents) => (
+          // return (
+<tr key={Documents.indexNo}>
+            <td>{Documents.indexNo}</td>
+            <td>{Documents.documentName}</td>
+            <td>{Documents.documentSize}</td>
+            <td>{Documents.owner}</td>
+          </tr>
+          // )
+          
+        ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
